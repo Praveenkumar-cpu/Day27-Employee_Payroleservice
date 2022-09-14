2ddedc34c53bcf60ac38cbbf4a31b5d4886ccacf
praveen <praveenpkbadiger@gmail.com>
2022-09-14 14:04:48 +0530

    Added: AllFile-Opeartion

---
+++ main/java/com/bridgelabz/EmployPayrollService.java
@@ -0,0 +1,72 @@
+package com.bridgelabz;
+
+import java.io.IOException;
+import java.util.List;
+
+public class EmployPayrollService {
+
+    static List<EmployPayrollData> employList;
+
+    public enum IOServices{
+        CONSOLE_IO, FILE_IO
+    }
+
+    public EmployPayrollService() {
+    }
+
+    public EmployPayrollService(List<EmployPayrollData> employList) {
+        EmployPayrollService.employList =employList;
+    }
+//    create write method to print data back to console
+    public void writeEmployPayrollData(IOServices ioServices){
+        PayrollService payrollService = getEmployeePayrollObject(ioServices);
+        try {
+            payrollService.writePayrollData(employList);
+        } catch (IOException e) {
+            System.out.println("catch block" + e);
+        }
+    }
+    public void readEmployPayrollData(IOServices ioServices) {
+        PayrollService payrollService = getEmployeePayrollObject(ioServices);
+        try {
+            payrollService.readPayrollData();
+        } catch (IOException e) {
+            System.out.println("catch block" + e);
+        }
+    }
+    public long countEntries(IOServices ioServices) {
+        PayrollService payrollService = getEmployeePayrollObject(ioServices);
+        try {
+            return payrollService.countEntries();
+        } catch (IOException e) {
+            System.out.println("catch block" + e);
+        }
+        return 0;
+    }
+    private static PayrollService getEmployeePayrollObject(IOServices ioServices){
+        PayrollService payrollService = null;
+        if(IOServices.FILE_IO.equals(ioServices))
+            payrollService = new FileIOPayrollService();
+        else if(IOServices.CONSOLE_IO.equals(ioServices))
+            payrollService = new ConsoleIOPayrollService();
+        return payrollService;
+    }
+
+//    method to read the input from user through console
+    public static void main(String[] args) {
+        EmployPayrollService employPayrollService = new EmployPayrollService();
+        employPayrollService.readEmployPayrollData(IOServices.CONSOLE_IO);
+        employPayrollService.writeEmployPayrollData(IOServices.CONSOLE_IO);
+        long count= employPayrollService.countEntries(IOServices.CONSOLE_IO);
+//        Show FileIO count no of entries.
+        System.out.println(count);
+        FileIOPayrollService fileIOPayrollService = new FileIOPayrollService();
+        try {
+            List<String> list= fileIOPayrollService.readData();
+            System.out.println(list);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
---
+++ main/java/com/bridgelabz/FileIOPayrollService.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileIOPayrollService implements PayrollService {
+    private final String FILE_PATH = "F:\\Day27-EmployeePayRole-Service\\src\\main\\java\\com\\bridgelabz\\File01";
+
+    @Override
+    public boolean writePayrollData(List<EmployPayrollData> employeeDataList) throws IOException {
+        StringBuffer employBuffer = new StringBuffer();
+        employeeDataList.forEach(employData ->{
+            String employDataString = employData.toString().concat("\n");
+            employBuffer.append(employDataString);
+        } );
+        Path path = Files.write(Paths.get(FILE_PATH), employBuffer.toString().getBytes());
+        return path !=null ? true :false;
+    }
+
+    @Override
+    public void readPayrollData() throws IOException {
+        Files.lines(new File(FILE_PATH).toPath()).forEach(System.out::println);
+    }
+
+    @Override
+    public long countEntries() throws IOException {
+        return Files.lines(new File(FILE_PATH).toPath()).count();
+    }
+    public List<String> readData() throws IOException {
+        List<String> employeePayrollList = new ArrayList<>();
+            Files.lines(new File(FILE_PATH).toPath()).map(String::trim)
+                    .forEach(employeePayrollList::add);
+        return employeePayrollList;
+    }
+}
